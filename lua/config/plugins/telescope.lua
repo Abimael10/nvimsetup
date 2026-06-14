bd884945cf7edec5c32b999f7e5ec48a04416785
abimael10 <juanabimael@gmail.com>
2026-06-14 18:08:45 -0400

    adding telescope live grep keymaps (<leader>fg/fw) for project-wide regex search

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -19,6 +19,28 @@ return {
         desc = "Find directory",
       },
       {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Grep (live, regex)",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("telescope.builtin").grep_string()
+        end,
+        desc = "Grep word under cursor",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("telescope.builtin").grep_string()
+        end,
+        mode = "v",
+        desc = "Grep selection",
+      },
+      {
         "<leader>en",
         function()
           require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
